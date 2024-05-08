e090d110d3312b2284cfb4e0ef97ae078d2e7ac9
mariocraft <154646419+mariocraft987@users.noreply.github.com>
2024-05-08 09:34:59 -0700

    Delete src directory

--- src/scripts/index.js
+++
@@ -1,4 +0,0 @@
-function myFunction() {
-    let message = document.getElementById('code').value
-    location.href = "https://github.com/mariocraft987/bark.github.io/issues/new?template=bug_report&labels=comment&title=A%20message%20to%20the%20dev&body=" + message;
-}
